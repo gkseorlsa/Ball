fa029dd957925a19853be298cd590d47dd66f701
bam <juhyun19981129@gmail.com>
2025-07-07 19:47:28 +0900

    chore: JwtAuthenticationFilter 추가

---
+++ backend/src/main/java/com/joo/ball/util/security/JwtAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.joo.ball.util.security;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request,
+        @NonNull HttpServletResponse response,
+        @NonNull FilterChain filterChain)
+        throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorizationHeader.substring(7);
+        String username = null;
+
+        try {
+            username = jwtUtil.extractUsername(token);
+        } catch (ExpiredJwtException | MalformedJwtException | SignatureException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Invalid or expired JWT token");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- backend/src/main/java/com/joo/ball/util/security/JwtUtil.java
+++ backend/src/main/java/com/joo/ball/util/security/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.joo.ball.util.security;
 
 import com.joo.ball.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -39,5 +40,18 @@ public class JwtUtil {
             .setExpiration(new Date(System.currentTimeMillis() + expiration))
             .signWith(key, SignatureAlgorithm.HS256)
             .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+            .parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
     }
 }
