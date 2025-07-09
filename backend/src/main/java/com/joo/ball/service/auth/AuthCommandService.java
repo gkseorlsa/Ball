d2c4039d77d6d4d2681b7ade994d8f14c21f4375
Daehan <gkseorlsa@gmail.com>
2025-07-09 17:44:36 +0900

    feat: 회원가입 구현

--- backend/src/main/java/com/joo/ball/service/auth/AuthCommandService.java
+++ backend/src/main/java/com/joo/ball/service/auth/AuthCommandService.java
@@ -1,16 +1,32 @@
 package com.joo.ball.service.auth;
 
 import com.joo.ball.dto.auth.request.SignUpRequest;
+import com.joo.ball.entity.user.User;
+import com.joo.ball.factory.UserFactory;
+import com.joo.ball.repository.user.UserRepository;
+import com.joo.ball.util.exception.DuplicateUsernameException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class AuthCommandService {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	@Transactional
     public void createUser(SignUpRequest signUpRequest) {
-        /*
-        * 회원 생성
-        * 1. 중복된 username이 있나 체크
-        * 2. 중복 발견 시 실패 반환
-        * 3. 중복 아닐 시 password 암호화하고 DB에 저장
-         */
+		userRepository.findByUsername(signUpRequest.username())
+			.ifPresent(user -> {
+				throw new DuplicateUsernameException("이미 존재하는 계정입니다.");
+			});
+
+		String encryptedPassword = passwordEncoder.encode(signUpRequest.password());
+
+		User newUser = UserFactory.of(signUpRequest, encryptedPassword);
+        userRepository.save(newUser);
     }
 }
